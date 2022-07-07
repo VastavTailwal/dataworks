7518e9229d8da917dc619919ea6aa07b4f2a811c
Vastav Tailwal <90317011+VastavTailwal@users.noreply.github.com>
2022-07-07 10:32:49 +0530

    added documentation

--- hello.cpp
+++ hello.cpp
@@ -1,15 +1,24 @@
-#include<iostream>
+#include<iostream>    // attached a header file
 
 
 int main()
 {
+	
+	/* declaring variables */
+	
 	unsigned short int roll_num;
 	char name[32];
+	
+	/* taking user input */
+	
 	std::cout << "Hello World" << std::endl;
 	std::cout << "Enter name :";
 	std::cin.getline(name);
 	std::cout << "Enter roll number :" << std::endl;
 	std::cin >> roll_num;
+	
+	/* printing user input on standart output (console window) */
+	
 	std::cout << name << "your roll number is " << roll_num;
 	return 0;
 }
