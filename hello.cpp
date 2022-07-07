a9af7be6a072bd2eac353517b0418bfdf019a8bd
vastav tailwal <tailwalvastav@gmail.com>
2022-07-07 09:20:18 +0530

    added roll num and name

--- hello.cpp
+++ hello.cpp
@@ -1,9 +1,15 @@
 #include<iostream>
-using namespace std;
 
 
 int main()
 {
-	cout << "Hello World";
+	unsigned short int roll_num;
+	char name[32];
+	std::cout << "Hello World" << std::endl;
+	std::cout << "Enter name :";
+	std::cin.getline(name);
+	std::cout << "Enter roll number :" << std::endl;
+	std::cin >> roll_num;
+	std::cout << name << "your roll number is " << roll_num;
 	return 0;
 }
--- hello.py
+++ hello.py
@@ -1,1 +1,5 @@
-print("Hello World")
+from sys import stdout as outfile
+
+
+roll_num, name = int(input("Enter roll number :"), input("Enter name :")
+print("Hello World," + name + "your roll number is" + str(roll_num), sep = ' ', end = '\n', flush = True, file = outfile)
