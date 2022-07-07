573e610554fcd838fb7cb154684cd9ecc4a87572
Vastav Tailwal <90317011+VastavTailwal@users.noreply.github.com>
2022-07-07 10:30:00 +0530

    added documentation

--- hello.py
+++ hello.py
@@ -1,5 +1,6 @@
-from sys import stdout as outfile
+from sys import stdout as outfile    # added stdout as outfile from sys module
 
 
-roll_num, name = int(input("Enter roll number :"), input("Enter name :")
+roll_num, name = int(input("Enter roll number :"), input("Enter name :")          # took user input for roll number and name
 print("Hello World," + name + "your roll number is" + str(roll_num), sep = ' ', end = '\n', flush = True, file = outfile)
+''' print name and roll number on standard output without loading it into buffer '''
