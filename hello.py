56c8dd02baa8258a4a8b04204019308314e5bbd7
vastav tailwal <tailwalvastav@gmail.com>
2022-07-12 21:24:54 +0530

    added comment to explain multiline comment

--- hello.py
+++ hello.py
@@ -3,4 +3,5 @@ from sys import stdout as outfile    # added stdout as outfile from sys module
 
 roll_num, name = int(input("Enter roll number :"), input("Enter name :")          # took user input for roll number and name
 print("Hello World," + name + "your roll number is" + str(roll_num), sep = ' ', end = '\n', flush = True, file = outfile)
+                  # multiline comment
 ''' print name and roll number on standard output without loading it into buffer '''
